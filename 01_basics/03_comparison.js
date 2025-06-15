c28eaa27f5d15904ecbe2ce465e1a9bd509348b6
Ayodhya <ayodhyarode03>
2025-06-15 17:08:28 +0530

    Cmparison Operators of JS

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,22 @@
+// console.log(1>11);      // F
+// console.log(1<11);         // T
+// console.log(11!=31);       // T
+
+
+// console.log('2'>1);         // T
+// console.log('2'>2);             // F
+// console.log('02'>2);             // F
+
+
+// === e
+// == → Loose Equality      Compares values only // Converts (coerces) types if needed
+
+//  === → Strict Equality  // Compares both value and type , No type conversion
+
+// Data Types
+
+//1] Primitive
+//  string,number,boolean,null,undefind,Symbol,BigInt
+
+//  2] Non-primitive
+// Array, object, functions
