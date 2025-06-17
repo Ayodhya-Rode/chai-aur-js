465ab2ace758335ca9505d05d4e2a394c109b908
Ayodhya <ayodhyarode03>
2025-06-17 12:15:42 +0530

    Updated 05_array.js with new changes

--- 02_basics/05_array.js
+++ 02_basics/05_array.js
@@ -5,7 +5,7 @@ let arr = [1,2,3,4]
 
 // ARRAY METHODS
 
-// 1] PUSH(ele)        // add at last
+// 1] PUSH(ele)        // add at last // modify original    // purn array la 1 index madhe add karat
 arr.push(5)
 // console.log(arr);
 
@@ -32,7 +32,7 @@ arr.shift()
 // console.log(newArr);
 // console.log(arr);
 
-//  8] concat(arrays)    merge 2 or more arrays/values into new array—without changing the original ones.
+//  8] concat(arrays) //spreads elements individually // merge arrays into new array— without changing the original ones.
 // let arr2 = [7,8,8]
 // let w = [9,9,9]
 // let newArray = arr.concat(arr2,w)
@@ -49,3 +49,40 @@ arr.shift()
 // console.log('before ' + names);
 // console.log(names.splice(1,3));
 // console.log('after ' + names);
+
+// 11] spread(...)  //expand elements of array or object into individual elements. //shallow copy
+
+// 12] flat  // returns new array by removing nested array layers up to the given depth.
+// Does not change the original.
+
+// let a = [1,2,3,[9,7,6],[5,6,[7,8,9]],[0,0]]
+// let b = a.flat(1)
+// console.log(b);
+// console.log(a);
+
+// array.flat(1),array.flat(2), array.flat(infinity)
+
+
+// 13] Array.isArray()  //checks whether a value is an array.  //bool
+// let a = [1,2,3,[9,7,6],[5,6,[7,8,9]],[0,0]]              //true
+// console.log(Array.isArray(a));
+
+// let a ={1:1}                         //false
+// console.log(Array.isArray(a));
+
+
+// 14] Array.from(val)  // converts array-like or iterable object into array. // transforming something INTO an array
+// console.log(Array.from(['small']));   //just creates a shallow copy  //['small']
+// console.log(Array.from('small'));     //convert string to array //[ 's', 'm', 'a', 'l', 'l' ]
+// console.log(Array.from({name:'Ayodhya'}));  //[]
+
+// 15] Array.of(val)    //Creates an array from given values //simply MAKES an array from given values
+// console.log(Array.of('hi'));    //[ 'hi' ]
+// console.log(Array.from('hi'));  //[ 'h', 'i' ]
+
+// console.log(Array.of('hi','ui','ji'));  //[ 'hi', 'ui', 'ji' ]
+// console.log(Array.from('hiii','ui','ji'));  //eroor
+
+
+// Think of Array.from() as transforming something INTO an array, while Array.of() simply MAKES an array from given values.
+
